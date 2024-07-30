f57b121ea18c3e125a5099b7e6c9948e7409ee06
dawit <dawitxxd@gmail.com>
2024-07-30 11:37:23 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString || !(set instanceof Set) || !(startString instanceof String)) {
+  const results = [];
+  if (!set || !startString || !(set instanceof Set) || !(typeof startString === 'string')) {
     return '';
   }
-  const results = [];
+  
   for (const item of set) {
     if (typeof item === 'string' && item.startsWith(startString)) {
       results.push(item.substring(startString.length));
