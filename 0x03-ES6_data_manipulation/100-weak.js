77cf574e6db38c3f3407758e175a6afa420d2b63
dawit <dawitxxd@gmail.com>
2024-07-30 23:03:41 +0300

    Weak map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,12 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  if (!(weakMap.get(endpoint))) {
+  if (!(weakMap.has(endpoint))) {
     weakMap.set(endpoint, 1);
   } else {
     const value = weakMap.get(endpoint);
+    weakMap.set(endpoint, value + 1);
     if (value >= 5) {
       throw new Error('Endpoint load is high');
     }
-    weakMap.set(endpoint, value + 1);
   }
 }
