0a6f6b6b104919e7eb045c973ab05c4329add86d
dawit <dawitxxd@gmail.com>
2024-09-20 16:41:21 +0300

    Basic test with mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,14 +4,14 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', function () {
   it('returns correct sum', function () {
     const result = calculateNumber(1, 3);
-    assert(result, 4);
+    assert.strictEqual(result, 4);
   });
   it('rounds and returns correct sum', function () {
     const result = calculateNumber(1.2, 3.7);
-    assert(result, 5);
+    assert.strictEqual(result, 5);
   });
   it('returns correct sum', function () {
     const result = calculateNumber(1.5, 3.7);
-    assert(result, 6);
+    assert.strictEqual(result, 6);
   });
 });
