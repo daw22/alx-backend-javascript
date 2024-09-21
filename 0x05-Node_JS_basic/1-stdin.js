f6b3ec297706a2e8e925603c9b51d5af4c5152c5
dawit <dawitxxd@gmail.com>
2024-09-21 11:02:16 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdin.on('end', () => {
   process.exit(0);
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
   process.exit(0);
